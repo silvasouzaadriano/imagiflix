ccc929a59f8333cc11cb6f357fe72a384690f2bc
adriano.souza <adriano.souza@zpesystems.com>
2022-11-13 20:36:49 -0300

    feat(hero): Implementing Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 
-function App() {
+import Hero from './components/Hero';
+
+const App = ()  => {
   return (
-    <div className="m-auto antialiased font-sans text-center">
-      <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
-        <img src={logo} className="h-48" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-300"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='m-auto antialised font-sans bg-black text-white'>
+      <Hero />
     </div>
   );
 }
