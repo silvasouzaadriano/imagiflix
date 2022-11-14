0b64f677bd3c2120ece3f791a1529c00c789c563
adriano.souza <adriano.souza@zpesystems.com>
2022-11-13 21:01:54 -0300

    feat(navbar): Implementing NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Hero from './components/Hero';
+import NavBar from './components/NavBar';
 
 const App = ()  => {
   return (
     <div className='m-auto antialised font-sans bg-black text-white'>
       <Hero />
+      <NavBar />
     </div>
   );
 }
