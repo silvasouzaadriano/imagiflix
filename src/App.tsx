93688de4130a87bcac23cc7a45872d63a434c984
adriano.souza <adriano.souza@zpesystems.com>
2022-11-23 22:24:39 -0300

    feat(hero): fetched movie from DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { URL, APISTRING } from './data/contants'
 
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
@@ -8,9 +10,23 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const App = ()  => {
+  const [ movies, setMovies ] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    const response = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
+    const moviesData = await response.json();
+    setMovies(moviesData.results);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+   
+
   return (
     <div className='m-auto antialised font-sans bg-black text-white'>
-      <Hero />
+      <Hero {...movies[0] } />
       <NavBar />
       <Carousel />
       <Carousel />
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,11 +6,14 @@ import Score from '../Score';
 
 import placeholder from '../../assets/hero.jpg';
 
-const Hero = ({ title = 'Avengers Endgame', score = 10 }) => (
+import { IMAGEURL } from '../../data/contants';
+
+const Hero = ({ backdrop_path = null, title = 'Avengers Endgame', vote_average  = 10 }) => {
+  return (
     <header className='box-border relative min-h-screen -mb-32'>
       <img
         className='object-cover object-center h-auto w-full'
-        src={placeholder}
+        src={backdrop_path ? `${IMAGEURL}/original/${backdrop_path}` : placeholder}
         alt='Filme em destaque'
       />
       <div className='absolute left-0 bottom-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
@@ -19,7 +22,7 @@ const Hero = ({ title = 'Avengers Endgame', score = 10 }) => (
         <h2 className='text-6xl font-bold'>{title}</h2>
         <p className='text-base'>
           Nota{' '}
-          <Score value={score} />
+          <Score value={vote_average } />
         </p>
         <button className='text-base py-2 px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-300 ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
           <FontAwesomeIcon className='mr-2' icon={faPlay} /> Assistir
@@ -29,7 +32,8 @@ const Hero = ({ title = 'Avengers Endgame', score = 10 }) => (
         </button>
       </article>
     </header>
- );
+ )
+};
 
 
 export default Hero;
