04b226ee72813cb3e4bd1247517a6e87152804d2
adriano.souza <adriano.souza@zpesystems.com>
2022-11-24 19:31:51 -0300

    feat(hero): Refactoring App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
+
 import { URL, APISTRING } from './data/contants'
 
 import Hero from './components/Hero';
@@ -9,20 +12,23 @@ import Carousel from './components/Carousel';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const App = ()  => {
+const App = () => {
   const [ movies, setMovies ] = useState<any[]>([]);
+  const url = `${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`
 
-  const fetchData = async () => {
-    const response = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
-    const moviesData = await response.json();
-    setMovies(moviesData.results);
-  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setMovies(response.data.results);
+      } catch (error) {
+        setMovies([])
+      }
+    }
 
-   
+    fetchData();
+  }, [url]);
 
   return (
     <div className='m-auto antialised font-sans bg-black text-white'>
@@ -33,6 +39,6 @@ const App = ()  => {
       <Carousel />
     </div>
   );
-}
+};
 
 export default App;
